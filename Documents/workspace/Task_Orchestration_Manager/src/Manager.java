5936b0f01376b8e2c8970d3fb97546010df95909
William O <wvo2@case.edu>
2017-04-15 18:14:53 -0400

    added Manager, finishing up scaffolding to start building actual functionality

---
+++ Documents/workspace/Task_Orchestration_Manager/src/Manager.java
@@ -0,0 +1,17 @@
+import java.util.GregorianCalendar;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Manager 
+{
+ArrayList<Task> AllTasks = new ArrayList<Task>();
+ArrayList<Task> PossibleTasks = new ArrayList<Task>();
+ArrayList<Day> AllHistory = new ArrayList<Day>();
+
+
+public static void Main(String [] args)
+{
+	
+}
+
+}
