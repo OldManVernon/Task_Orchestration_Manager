abce8b0f9fb175e4dc840f0b1fadc99dae4c6051
William O <wvo2@case.edu>
2017-04-15 14:37:08 -0400

    classes to get the task manager project rolling

---
+++ Documents/workspace/Task_Orchestration_Manager/src/Task.java
@@ -0,0 +1,19 @@
+import java.util.GregorianCalendar;
+import java.util.ArrayList;
+
+public class Task 
+{
+private String name;
+private ArrayList<String> Supples;
+private int Category;
+private GregorianCalendar deadline;
+private int id;
+private int completion;
+private ArrayList<Event> history;
+private ArrayList<GregorianCalendar> Blackout;
+private int priority;
+private int priority_growth;
+private ArrayList<Task> subtasks;
+private GregorianCalendar timer;
+
+}
